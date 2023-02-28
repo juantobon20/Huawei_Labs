6227c0f96cf04eab9ff3b8c514846a01c52ade64
juantobonq <101196521+juantobonq@users.noreply.github.com>
2023-02-28 08:47:55 -0500

    adasdda

--- app/src/main/java/com/appinc/myapplication/MainActivity.kt
+++ app/src/main/java/com/appinc/myapplication/MainActivity.kt
@@ -2,17 +2,36 @@ package com.appinc.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.hms.aaid.HmsInstanceId
 import com.huawei.hms.maps.SupportMapFragment
 import com.huawei.hms.maps.model.CircleOptions
 import com.huawei.hms.maps.model.LatLng
 import com.huawei.hms.maps.model.MarkerOptions
 import com.huawei.hms.maps.model.PolygonOptions
 import com.huawei.hms.maps.model.PolylineOptions
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        thread {
+            try {
+                val appId = AGConnectServicesConfig.fromContext(this).getString("client/app_id")
+                val token = HmsInstanceId.getInstance(this).getToken(appId, "HCM")
+                Log.i("Token", token)
+
+                Toast.makeText(this, token, Toast.LENGTH_LONG).show()
+                Log.e("HuaweiDEMO", token)
+            } catch (ex: Exception) {
+                Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
+                Log.e("HuaweiDEMO", "${ex.message}")
+            }
+        }.start()
 
         val mapView = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
         mapView.getMapAsync { map ->
---
+++ app/src/main/java/com/appinc/myapplication/NotificationServices.kt
@@ -0,0 +1,21 @@
+package com.appinc.myapplication
+
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.push.HmsMessageService
+import com.huawei.hms.support.api.push.PushMsgReceiver
+
+class NotificationServices : HmsMessageService() {
+
+
+    override fun onMessageSent(p0: String?) {
+        super.onMessageSent(p0)
+
+
+
+
+    }
+}
